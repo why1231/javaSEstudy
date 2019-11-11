d8e799750548f6e502f9a8e99fd25818392e691c
why1231 <1757751022@qq.com>
2019-11-11 22:18:06 +0800

    failed binarytree

---
+++ datastructure/src/main/datasutructure/BinaryTree.java
@@ -0,0 +1,17 @@
+package main.datasutructure;
+
+public class BinaryTree {
+    public int value ;
+    public BinaryTree leftChild;
+    public BinaryTree rightChild;
+
+    public BinaryTree(int value){
+        this.value = value;
+    }
+
+    //Ôö
+    public int add(int value){
+
+        return value;
+    }
+}
