659628f8e93b933ab81bd28b8100878cc3d9ca3c
why1231 <1757751022@qq.com>
2019-11-17 01:27:51 +0800

    两个数之和

--- algorithm/src/algorithm/likou/TwoNumberAnd.java
+++ algorithm/src/algorithm/likou/TwoNumberAnd.java
@@ -1,28 +1,33 @@
+/***
+ * 两数之和的问题，但是我审题错了第一个类是我的解决方案，没有解决以零开头的问题
+ * 第二个类是官方的
+ */
+
 package algorithm.likou;
 
 import java.util.Arrays;
 
 public class TwoNumberAnd {
 
-    public static int[] twoNum(int[] arr,int target){
+    public static int[] twoNum(int[] arr, int target) {
         int length = arr.length;
-        int[] result = new int [length];
-        int[] check = new int [length];
+        int[] result = new int[length];
+        int[] check = new int[length];
         int count = 0;
         int index = 0;
         int temp = 0;
         boolean flag = false;
-        for(int i = 0;i<length-1;i++){
-            temp = target-arr[i];
+        for (int i = 0; i < length - 1; i++) {
+            temp = target - arr[i];
             index = i;
-            for(int j = 0;j<length;j++){
-                if(check[j] == arr[i])  flag = true;
+            for (int j = 0; j < length; j++) {
+                if (check[j] == arr[i]) flag = true;
             }
-            if(!flag){
-                for(int k = 0 ;k<length;k++){
-                    if(arr[k]==temp){
+            if (!flag) {
+                for (int k = i + 1; k < length; k++) {
+                    if (arr[k] == temp) {
                         result[count] = i;
-                        check[count]=arr[i];
+                        check[count] = arr[i];
                         count++;
                         result[count] = k;
                         check[count] = arr[k];
@@ -32,13 +37,36 @@ public class TwoNumberAnd {
             }
             flag = false;
         }
-        return result;
+        int[] finish = new int[count];
+        for (int m = 0; m < count; m++) {
+            finish[m] = result[m];
+        }
+        return finish;
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7,8,9};
-        int target = 7;
-        System.out.println(Arrays.toString(twoNum(arr,target)));
+        int[] arr = {0, 4, 3, 0};
+        int target = 0;
+        System.out.println(Arrays.toString(twoNum(arr, target)));
     }
 
+}
+
+class TwoNum{
+    public static int[] twoNum(int[] arr,int target){
+        for(int i = 0 ;i<arr.length-1;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]==target-arr[i]){
+                    return new int[] {i,j};
+                }
+            }
+        }
+        throw new RuntimeException("不存在两个数的和等于target");
+    }
+
+    public static void main(String[] args) {
+        int[] arr= {1,4,8,29,78};
+        int target = 86;
+        System.out.println(Arrays.toString(twoNum(arr,target)));
+    }
 }
